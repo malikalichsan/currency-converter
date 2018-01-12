7ef628f5d44c889b4f4faa64ad19b0c1e9e76bb8
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-12 16:35:05 +0700

    Convert Dollar to Rp

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public void convertFunc(View view) {
-
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+        double amountDouble = Double.parseDouble(amountEditText.getText().toString());
+        double result = amountDouble * 13354;
+        Toast.makeText(this, "Rp. " + result, Toast.LENGTH_SHORT).show();
     }
 
     @Override
