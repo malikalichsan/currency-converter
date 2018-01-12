276763ebed717029f3ebb0a7e6a43dd345bcf6f0
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-12 15:47:46 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void convertFunc(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
